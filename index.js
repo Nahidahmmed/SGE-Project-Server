3fbd9b1cf55601d16a532802a4d39e2c3ffc9f0b
Nahidahmmed <nahidahmmed411@gmail.com>
2024-05-29 11:55:25 +0600

    jwt

--- index.js
+++ index.js
@@ -28,8 +28,42 @@ async function run() {
 
     const userCollection = client.db("SGE-Server").collection("Users");
 
-    app.get('/users', async (req, res) => {
-      const result = await userCollection.find().toArray();
+    // jwt api
+    app.post('/jwt', async(req,res) =>{
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1h' });
+      res.send({token});
+    })
+
+    // middlewares
+    const verifyToken = (req, res, next) => {
+      console.log(req.headers);
+      if(!req.headers.authorization){
+        return res.status(401).send({message: 'forbidden access'})
+      }
+      const token = req.headers.authorization.split(' ')[1];
+     
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) =>{
+        if(err){
+          return res.status(401).send({message: 'forbidden access'})
+        }
+        req.decoded = decoded;
+        next();
+      })
+    }
+
+    // app.get('/users', async (req, res) => {
+    //   const result = await userCollection.find().toArray();
+    //   res.send(result);
+    // });
+    app.post('/users', async (req, res) => {
+      const user = req.body;
+      const query = {email: user.email}
+      const existingUser = await userCollection.findOne(query);
+      if(existingUser){
+        return res.send({message: 'user already exists', insertedId: null})
+      }
+      const result = await userCollection.insertOne(user);
       res.send(result);
     });
 
